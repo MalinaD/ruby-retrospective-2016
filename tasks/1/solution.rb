a38c461f5fc6176f0a30d7bfee5c429b9526c3da
malinad <malina.demirova@gmail.com>
2017-01-15 22:08:03 +0200

    Task 1 Changing the formula

--- tasks/1/solution.rb
+++ tasks/1/solution.rb
@@ -1,29 +1,36 @@
-def convert_between_temperature_units(degrees, unit_degrees, unit_result)
-  if unit_result == 'C' && unit_degrees == 'F'
-    ((degrees - 32) / 1.8).round(2)
-  elsif unit_result == 'F' && unit_degrees == 'C'
-    (degrees * 1.8 + 32).round(2)
-  elsif unit_result == 'K' && unit_degrees == 'C'
-    (degrees + 273.15).round(2)
+MELTING_POINT = {
+  'water' => 0,
+  'ethanol' => -114,
+  'gold' => 1_064,
+  'silver' => 961.8,
+  'copper' => 1_085
+}
+
+BOILING_POINT = {
+  'water' => 100,
+  'ethanol' => 78.37,
+  'gold' => 2_700,
+  'silver' => 2_162,
+  'copper' => 2_567
+}
+
+def convert_between_temperature_units(degree, unit_degrees, unit_result)
+  if unit_degrees == unit_result
+    degree
   else
-    (degrees - 273.15).round(2)
+    convert_to_c_from = { 'K' => degree - 273.15, 'F' => (degree - 32) / 1.8, 'C' => degree }
+    convert_to = {
+      'C' => convert_to_c_from[unit_degrees],
+      'F' => convert_to_c_from[unit_degrees] * 1.8 + 32,
+      'K' => convert_to_c_from[unit_degrees] + 273.15
+    }
+    convert_to[unit_result]
   end
 end
+
 def melting_point_of_substance(substance, unit_degrees)
-  substances = { water: 0, ethanol: -114, gold: 1064, silver: 961.8, copper: 1085 }
-  
-  if unit_degrees == 'C'
-    substances[substance].to_i
-  else
-    convert_between_temperature_units(substances[substance].to_i, 'C', unit_degrees)
-  end
+  convert_between_temperature_units(MELTING_POINT[substance].to_i, 'C', unit_degrees)
 end
 def boiling_point_of_substance(substance, unit_degrees)
-  substances = { water: 100, ethanol: 78.37, gold: 2700, silver: 2162, copper: 2567 }
-    
-  if unit_degrees == 'C'
-    substances[substance].to_i
-  else
-    convert_between_temperature_units(substances[substance].to_i, 'C', unit_degrees)
-  end
+  convert_between_temperature_units(BOILING_POINT[substance].to_i, 'C', unit_degrees)
 end
